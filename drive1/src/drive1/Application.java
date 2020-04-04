22887ae3de367ebbeb3386f74f5b05a9a91f5a57
simmeko26 <simretmekonen380@gmail.com>
2020-04-03 20:28:25 -0400

    county baltimore

---
+++ drive1/src/drive1/Application.java
@@ -0,0 +1,13 @@
+package drive1;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mynumbers=10;
+		System.out.print(mynumbers);
+		
+
+	}
+
+}
